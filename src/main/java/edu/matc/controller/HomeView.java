1a303a89e4ea7f7717a9643a7472fae1a28bfe81
timzschernitz <tzschernitz@madisoncollege.edu>
2021-03-20 18:58:37 -0500

    enhanced authentication/security surrounding login and reaching the home page

---
+++ src/main/java/edu/matc/controller/HomeView.java
@@ -0,0 +1,56 @@
+package edu.matc.controller;
+
+import edu.matc.entity.User;
+import edu.matc.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * A simple servlet to welcome the user.
+ * @author tzschernitz
+ */
+
+@WebServlet(
+        urlPatterns = {"/user/home"}
+)
+
+public class HomeView extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        System.out.println("Homeview servlet being hit.");
+
+//        GenericDao userDao = new GenericDao(User.class);
+
+        /* Determine if sufficient search criteria entered for specific search or to
+        / return all results from database.
+         */
+//        if (req.getParameter("searchType") != null) {
+//            if (req.getParameter("searchType").equals("id")) {
+//                List<User> user = new ArrayList<>();
+//                user.add((User) userDao.getById(parseInt(req.getParameter("searchTerm"))));
+//                req.setAttribute("users", user);
+//            } else if (req.getParameter("searchType").equals("last_name")) {
+//                req.setAttribute("users", userDao.getByPropertyLike("lastName", req.getParameter("searchTerm")));
+//            } else if (req.getParameter("searchType").equals("first_name")) {
+//                req.setAttribute("users", userDao.getByPropertyLike("firstName", req.getParameter("searchTerm")));
+//            }
+//        } else {
+//            req.setAttribute("users", userDao.getAll());
+//        }
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/user/home.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
--- src/main/java/edu/matc/controller/LoginSuccess.java
+++
@@ -1,4 +0,0 @@
-package edu.matc.controller;
-
-public class LoginSuccess {
-}
