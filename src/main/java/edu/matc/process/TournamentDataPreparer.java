bba34024526b6531d714361b26aab8144cd19562
timzschernitz <tzschernitz@madisoncollege.edu>
2021-05-01 14:39:21 -0500

    TournamentDataPreparer: worked out logic for parsing user data
     - The TournamentDataPreparer is functioning as expected by parsing out
     the user string data into separate key value pairings for each relavent
     value related to a users end game stats.
     - Still need to work in extracting the date the tournament took place.

--- src/main/java/edu/matc/process/TournamentDataPreparer.java
+++ src/main/java/edu/matc/process/TournamentDataPreparer.java
@@ -1,10 +1,9 @@
 package edu.matc.process;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TournamentDataPreparer {
 
@@ -13,41 +12,71 @@ public class TournamentDataPreparer {
 
     List<String> dataToPrepare;
     ArrayList<String[]> preparedData;
-    HashMap<String, String> finalPreparedData;
+    ArrayList<HashMap<String, String>> finalPreparedData;
 
     public TournamentDataPreparer() {}
 
     public TournamentDataPreparer(List<String> dataToPrepare) {
         this.dataToPrepare = dataToPrepare;
         preparedData = new ArrayList<>();
+        finalPreparedData = new ArrayList<>();
     }
 
     public ArrayList<String[]> go() {
-//        stripOutUnusedText(dataToPrepare, WORD_TO_REMOVE_FROM_USER_RESULT_DATA);
-        parseOutStringData(stripOutUnusedText(extractUserDataRows(dataToPrepare), WORD_TO_REMOVE_FROM_USER_RESULT_DATA));
+//        parseOutStringData(fixLabelsAndAddDelimiters(extractUserDataRows(dataToPrepare)));
+//
+//        System.out.println(preparedData.get(0)[0]);
+//        System.out.println(preparedData.get(0)[1]);
+//        System.out.println(preparedData.get(0)[2]);
+//        System.out.println(preparedData.get(0)[3]);
+//        System.out.println(preparedData.get(0)[4]);
+//        System.out.println(preparedData.get(0)[5]);
+//        System.out.println(preparedData.get(0)[6]);
+//        System.out.println(preparedData.get(0)[7]);
+//        System.out.println(preparedData.get(0)[8]);
+//        System.out.println(preparedData.get(0)[9]);
+//        System.out.println(preparedData.get(0)[10]);
+
+        assignKeyValuePairing(
+                parseOutStringData(
+                        fixLabelsAndAddDelimiters(
+                                extractUserDataRows(dataToPrepare))));
+
+        int indexTest = 0;
+        for (HashMap<String, String> test : finalPreparedData) {
+            indexTest++;
+
+            for (Map.Entry<String, String> test2 : test.entrySet()) {
+                System.out.println("player#" + indexTest + "==== " + test2.getKey() + " " + test2.getValue());
+            }
+        }
 
 
+//        System.out.println(finalPreparedData.get(0).get("user"));
 
 
-
-        System.out.println(preparedData.get(1)[0]);
-        System.out.println(preparedData.get(1)[1]);
-        System.out.println(preparedData.get(1)[2]);
-        System.out.println(preparedData.get(1)[3]);
-        System.out.println(preparedData.get(1)[4]);
-        System.out.println(preparedData.get(1)[5]);
-        System.out.println(preparedData.get(0)[0]);
-        System.out.println(preparedData.get(0)[1]);
-        System.out.println(preparedData.get(0)[2]);
-        System.out.println(preparedData.get(0)[3]);
-        System.out.println(preparedData.get(0)[4]);
-        System.out.println(preparedData.get(0)[5]);
         return preparedData;
     }
 
-//    public ArrayList<String[]> assignKeyValuePairing(ArrayList<String> dataToPrepare) {
-//
-//    }
+    public ArrayList<HashMap<String, String>> assignKeyValuePairing(ArrayList<String[]> dataToPrepare) {
+
+        HashMap<String, String> userTournamentData;
+
+        for (String[] userData : dataToPrepare) {
+            userTournamentData = new HashMap<>();
+            for (int index = 0; index < userData.length-1; index+=2) {
+                if (index == 2) {
+                    userTournamentData.put("User", userData[index]);
+                    index-=1;
+                } else {
+                    userTournamentData.put(userData[index], userData[index+1]);
+                }
+            }
+            finalPreparedData.add(userTournamentData);
+        }
+        this.finalPreparedData = finalPreparedData;
+        return finalPreparedData;
+    }
 
 
     public ArrayList<String[]> parseOutStringData(ArrayList<String> dataToPrepare) {
@@ -57,84 +86,46 @@ public class TournamentDataPreparer {
         return preparedData;
     }
 
-
+    /**
+     * This method filters the Tournament Result data and extracts just the rows
+     * relevant to individual user results.
+     *
+     * @param dataToPrepare
+     * @return
+     */
     public ArrayList<String> extractUserDataRows(List<String> dataToPrepare) {
-
         ArrayList<String> filteredData = new ArrayList<>();
-
         for (String resultDataRow : dataToPrepare) {
-            if (identifyIfIsUserResultDataRow(resultDataRow)) {
-//                preparedData.add(separateStringBySpaces(resultDataRow));
+            if (resultDataRow.startsWith("Place")) {
                 filteredData.add(resultDataRow);
             }
         }
         return filteredData;
     }
 
-
-
     /**
-     * This method extracts only the rows of data specific to users final tournament
-     * results.
-     */
-    public boolean identifyIfIsUserResultDataRow(String rowToCheck) {
-            if (rowToCheck.startsWith("Place")) {
-                return true;
-            }
-        return false;
-    }
-
-
-    /**
-     * This method splits a string into separate array values based upon spaces
-     * as the delimiter.
-     */
-//    public String[] separateStringBySpaces(String dataToSplit) {
-//        return dataToSplit.split("(\\s)");
-//    }
-
-    /**
-     * Splitting by space and equal sign
+     * Splits a string by equals signs (=), spaces ( ), and colons (:).
      * @param dataToSplit
      * @return
      */
     public String[] separateStringBySpaces(String dataToSplit) {
-        return dataToSplit.split("=|\\s");
+        return dataToSplit.split("=|\\s|:");
     }
 
     /**
-     * Removes unwanted word from string data
-     * @param dataToStripFrom
-     * @param textToStrip
+     * This method fixes the user data string rows by adding a label for Money Won
+     * data and also adds colons where there isn't one separating a label from
+     * a corresponding value.
+     *
+     * @param dataToFix
      * @return
      */
-    public ArrayList<String> stripOutUnusedText(ArrayList<String> dataToStripFrom, String textToStrip) {
-
+    public ArrayList<String> fixLabelsAndAddDelimiters(ArrayList<String> dataToFix) {
         ArrayList<String> strippedData = new ArrayList<>();
-
-        for (String lineToStripFrom : dataToStripFrom) {
-            strippedData.add(StringUtils.strip(lineToStripFrom, textToStrip));
+        for (String lineToStripFrom : dataToFix) {
+            strippedData.add(lineToStripFrom.replace("Place", "Place:").replace("(", "MoneyWon:("));
         }
         return strippedData;
     }
-
-
-    public void extractUsers() {
-        //extracts place finished and username
-        for (String[] t : preparedData) {
-            String[] tempData = t[0].split("=");
-
-            // extracts place finished
-//            System.out.println(tempData[0]);
-
-            // extracts username
-//            System.out.println(tempData[1]);
-
-        }
-    }
-
-
-
-
 
 }
